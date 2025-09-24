9f4786fd3caddc52e24650a05c45bea01112f34b
William <juniorwilliam2008@hotmail.com>
2025-09-23 21:33:41 -0500

    Se creo la clase ImpresionMenu y un ciclo while en la clase principal

--- src/main/java/com/mycompany/alquilerautos/AlquilerAutos.java
+++ src/main/java/com/mycompany/alquilerautos/AlquilerAutos.java
@@ -1,8 +1,20 @@
 package com.mycompany.alquilerautos;
 
+import java.util.Scanner;
+
 public class AlquilerAutos {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Scanner scanner = new Scanner(System.in);
+        int opcionMenu;
+        
+        System.out.println("Bienvenido Al Programa De Alquiler De Autos");
+        
+        do{
+            ImpresionMenu.printOpciones();
+            opcionMenu = scanner.nextInt();        
+        }while (opcionMenu != 0);
+        
     }
 }
